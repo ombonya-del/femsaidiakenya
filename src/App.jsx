ac3345dac239faf0da5a86f5737dc51851525935
VO <vo@VOs-MacBook-Pro.local>
2026-05-15 18:38:54 +0300

    Fix useEffect import in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import RedFlagTab from './RedFlag.jsx'
 import PetitionTab from './Petition.jsx'
 import ReportTab from './Report.jsx'
