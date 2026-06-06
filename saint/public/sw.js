4f30e83a8d412b0b1dc330391b5ba55b5384fac6
Victor Ombonya <ombonya@gmail.com>
2026-06-06 03:10:59 +0300

    fix: bump SW cache to saint-v2, force fresh PDF

--- saint/public/sw.js
+++ saint/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "saint-v1"
+const CACHE = "saint-v2"
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(["/", "/index.html"])))
   self.skipWaiting()
